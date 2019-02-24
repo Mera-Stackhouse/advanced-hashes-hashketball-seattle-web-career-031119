86bb9cd5467a21909d11b460f0ef26d3ad6d8c44
Mera Stackhouse <mera.stackhouse@gmail.com>
2019-02-24 03:23:51 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,5 +1,20 @@
 # Write your code here!
 
+def game_hash
+  game_hash = {
+    home: {
+      team_name: "Brooklyn Nets",
+      colors: ["Black", "White"],
+      players: {
+        "Alan Anderson" => {
+          number: "0",
+          shoe:"16",
+          points: "22"
+        }
+      }
+    },
+    away:
+  }
 
 
 
